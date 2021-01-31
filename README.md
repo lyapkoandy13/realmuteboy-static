455ac800b8f145fbb9dc90a55fee9f39da6446de
Andrew Lyapko <lyapkoandy13@gmail.com>
2021-01-31 21:29:57 +0200

    Updated video id, fixed readme image

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><a href="http://realmuteboy.tk/" target="_blank" rel="noopener noreferrer"><img width="100" src="https://decisive-apogee-250410.appspot.com/static/realmuteboy/img/favicon.ico" alt="Realmuteboy logo"></a></p>
+<p align="center"><a href="http://realmuteboy.tk/" target="_blank" rel="noopener noreferrer"><img width="100" src="https://raw.githubusercontent.com/lyapkoandy13/realmuteboy-static/master/static/img/favicon.ico" alt="Realmuteboy logo"></a></p>
 <h2 align="center">realmuteboy-static</h2>
 
 Realmuteboy landing page
--- static/js/index.js
+++ static/js/index.js
@@ -1,48 +1,50 @@
-var tag = document.createElement('script');
-if(location.protocol=="file:"){
-    console.log('file protocol');
-    tag.src = "https://www.youtube.com/player_api";
-}else{
-    tag.src = location.protocol+"//www.youtube.com/player_api";
+var tag = document.createElement("script");
+if (location.protocol == "file:") {
+  console.log("file protocol");
+  tag.src = "https://www.youtube.com/player_api";
+} else {
+  tag.src = location.protocol + "//www.youtube.com/player_api";
 }
 console.log(tag.src);
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 
 var player;
 function onYouTubePlayerAPIReady() {
-    player = new YT.Player('player', {
-        playerVars: { 'autoplay': 1, 'controls': 0,'autohide':1,'wmode':'opaque'},
-        videoId: 'hHW1oY26kxQ',
-        height: '100%',
-        width: '100%',
-    });
-
+  player = new YT.Player("player", {
+    playerVars: { autoplay: 1, controls: 0, autohide: 1, wmode: "opaque" },
+    videoId: "5qap5aO4i9A",
+    height: "100%",
+    width: "100%",
+  });
 }
 
 function toggleSound() {
-    if (player.isMuted()) {
-        player.unMute()
-        document.getElementById('muteVideo--off').style.display = "none";
-        document.getElementById('muteVideo--up').style.display = "block";
-    } else {
-        player.mute()
-        document.getElementById('muteVideo--up').style.display = "none";;
-        document.getElementById('muteVideo--off').style.display = "block";;
-    }
+  if (player.isMuted()) {
+    player.unMute();
+    document.getElementById("muteVideo--off").style.display = "none";
+    document.getElementById("muteVideo--up").style.display = "block";
+  } else {
+    player.mute();
+    document.getElementById("muteVideo--up").style.display = "none";
+    document.getElementById("muteVideo--off").style.display = "block";
+  }
 }
 
-document.getElementById('muteVideo').addEventListener("click", function(){    
-    toggleSound();
+document.getElementById("muteVideo").addEventListener("click", function () {
+  toggleSound();
 });
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener(
+  "keypress",
+  function (e) {
     if (e.keyCode !== 77) {
-        toggleSound();
+      toggleSound();
     }
-}, false);
+  },
+  false
+);
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    // player.playVideo();
+document.addEventListener("DOMContentLoaded", function (event) {
+  // player.playVideo();
 });
