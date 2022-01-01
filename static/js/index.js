37e7a6465e1542c3a20a72b5e1adca63870ff427
Andrew Lyapko <lyapkoandy13@gmail.com>
2022-01-01 21:01:52 +0200

    Add volume controls

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 var tag = document.createElement("script");
 if (location.protocol == "file:") {
-  console.log("file protocol");
   tag.src = "https://www.youtube.com/player_api";
 } else {
   tag.src = location.protocol + "//www.youtube.com/player_api";
@@ -19,32 +18,60 @@ function onYouTubePlayerAPIReady() {
   });
 }
 
-function toggleSound() {
-  if (player.isMuted()) {
-    player.unMute();
-    document.getElementById("muteVideo--off").style.display = "none";
-    document.getElementById("muteVideo--up").style.display = "block";
-  } else {
-    player.mute();
+const volumeControlInput = document.getElementById("volume-control");
+volumeControlInput.addEventListener("change", function (e) {
+  toggleVolume(e.target.value);
+});
+
+function changeVolumeIcon(muted) {
+  if (muted) {
     document.getElementById("muteVideo--up").style.display = "none";
     document.getElementById("muteVideo--off").style.display = "block";
+  } else {
+    document.getElementById("muteVideo--off").style.display = "none";
+    document.getElementById("muteVideo--up").style.display = "block";
+  }
+}
+
+let prevVolume = null;
+
+function toggleVolume(volume) {
+  if (volume) {
+    prevVolume = player.getVolume();
+    player.setVolume(volume);
+  }
+
+  if (player.isMuted()) {
+    if (volume !== "0") {
+      if (
+        typeof volume === "undefined" &&
+        prevVolume &&
+        player.getVolume() === 0
+      ) {
+        player.setVolume(prevVolume);
+        volumeControlInput.value = prevVolume;
+      }
+      player.unMute();
+      changeVolumeIcon(false);
+    }
+  } else {
+    if (!volume || volume === "0") {
+      player.mute();
+      changeVolumeIcon(true);
+    }
   }
 }
 
 document.getElementById("muteVideo").addEventListener("click", function () {
-  toggleSound();
+  toggleVolume();
 });
 
 window.addEventListener(
   "keypress",
   function (e) {
-    if (e.keyCode !== 77) {
-      toggleSound();
+    if (e.key !== 77) {
+      toggleVolume();
     }
   },
   false
 );
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  // player.playVideo();
-});
